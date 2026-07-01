29a4e7eafe9bb324cc0ca3bc2079d36b82ebdcca
Halim <lohalim53@gmail.com>
2026-07-01 16:13:05 +0200

    Add first Python basics script

---
+++ python-basics/day-02.py
@@ -0,0 +1,10 @@
+name = "Halim"
+goal = "Learn MLOps"
+hours_per_day = 4
+
+print("Name:", name)
+print("Goal:", goal)
+print("Daily study hours:", hours_per_day)
+
+weekly_hours = hours_per_day * 6
+print("Weekly study hours:", weekly_hours)
